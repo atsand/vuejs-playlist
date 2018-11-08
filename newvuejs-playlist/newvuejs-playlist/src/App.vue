a4786eeb607eefd1994c6501feededaca9c91547
Aaron <atsand23@gmail.com>
2018-11-08 18:03:40 -0500

    Slots Intro

--- newvuejs-playlist/newvuejs-playlist/src/App.vue
+++ newvuejs-playlist/newvuejs-playlist/src/App.vue
@@ -1,17 +1,20 @@
 <template>
-
+  <form-helper>
+    <h2 slot="slotTitle">{{ title }}</h2>
+    <p slot="slotParagraph">This is the slot paragraph</p>
+  </form-helper>
 </template>
 
 <script>
-
+import formHelper from './components/formHelper'
 
 export default {
   components:{
-    
+    'form-helper':formHelper
   },
   data () {
     return {
-      
+      title:'I am a dynamic slot title'
     }
   },
   methods:{
---
+++ newvuejs-playlist/newvuejs-playlist/src/components/formHelper.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <slot name="slotTitle"></slot>
+        <h1>I'm the formHelper title</h1>
+        <slot name="slotParagraph"></slot>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+  components:{
+    
+  },
+  data () {
+    return {
+      
+    }
+  },
+  methods:{
+    
+  }
+}
+</script>
+
+<style scoped>
+h1{
+    color:red
+}
+
+</style>
