9cb9f9a92b15b6a4b782343f2216945190c2bf3f
Aaron <atsand23@gmail.com>
2018-11-08 20:36:18 -0500

    Using axios to pull in collection of posts from test api

--- newvuejs-playlist/newvuejs-playlist/src/main.js
+++ newvuejs-playlist/newvuejs-playlist/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 export const bus = new Vue();
+Vue.use(VueAxios, axios)
 
 new Vue({
   el: '#app',
